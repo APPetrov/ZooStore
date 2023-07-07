2cf7ace441b711870bd560829f4dd44b693d56b1
APPetrov <74349727+APPetrov@users.noreply.github.com>
2023-07-07 11:55:38 +0300

    Connect project
    Connect intellij project with github

---
+++ bigProjectTinqIn/src/main/java/com/example/bigprojecttinqin/BigProjectTinqInApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigprojecttinqin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigProjectTinqInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigProjectTinqInApplication.class, args);
+    }
+
+}
---
+++ bigProjectTinqIn/src/test/java/com/example/bigprojecttinqin/BigProjectTinqInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigprojecttinqin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigProjectTinqInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
